264c61fca4287d88d84b3e023905f39ff53347f1
joseperezc <9531447499amail.com>
2025-01-19 15:09:55 -0600

    Cree register.php para enviar datos a la DB

---
+++ docker/www/public/register.php
@@ -0,0 +1,28 @@
+﻿<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once 'controllers/AuthController.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    echo "<pre>";
+    print_r($_POST);
+    echo "</pre>";
+
+    $username = $_POST['username'] ?? null;
+    $email = $_POST['email'] ?? null;
+    $password = $_POST['password'] ?? null;
+
+    $auth = new AuthController();
+    try {
+        if ($auth->register($username, $email, $password)) {
+            echo "¡Registro exitoso!";
+        } else {
+            echo "Error al registrar.";
+        }
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+?>
