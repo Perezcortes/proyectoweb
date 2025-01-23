69d8638069d2c54a999358d582757ff24d014d22
joseperezc <9531447499amail.com>
2025-01-23 17:43:10 -0600

    Agregue logica para envio de datos

--- docker/www/public/register.php
+++ docker/www/public/register.php
@@ -1,6 +1,7 @@
-﻿<?php
-session_start();
+<?php
 include_once 'controllers/AuthController.php';
+
+// No es necesario establecer el encabezado 'Content-Type: application/json' si no vas a usar JSON
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'] ?? null;
@@ -9,13 +10,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $auth = new AuthController();
     try {
-        if ($auth->register($username, $email, $password)) {
-            header("Location: login.html");
-            exit();
-        } else {
-            echo "Error al registrar.";
-        }
+        $success = $auth->register($username, $email, $password);
+        // Enviar un mensaje simple en texto plano
+        echo $success ? 'Registro exitoso.' : 'Error al registrar en la base de datos.';
     } catch (Exception $e) {
-        echo "Error: " . $e->getMessage();
+        // Enviar un mensaje de error simple
+        echo 'Error: ' . $e->getMessage();
     }
 }
