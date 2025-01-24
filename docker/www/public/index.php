4cc9fc0b00d11ed280f5db70c8a8b2e696acf7da
joseperezc <9531447499amail.com>
2025-01-24 08:35:35 -0600

    Sin relevancia

--- docker/www/public/index.php
+++ docker/www/public/index.php
@@ -23,8 +23,8 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item"><a class="nav-link" href="#">Citas</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Tatuadores</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">Productos</a></li>
+                <li class="nav-item"><a class="nav-link" href="./views/tatuadores.html">Tatuadores</a></li>
+                <li class="nav-item"><a class="nav-link" href="./views/productos.html">Productos</a></li>
                 <li class="nav-item"><a class="nav-link" href="./views/percing.html">Perforaciones</a></li>
             </ul>
         </div>
