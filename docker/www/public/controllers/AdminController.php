ef13f15583cd8ccf13616819df4a63c6601fd63f
joseperezc <9531447499amail.com>
2025-01-28 20:02:18 -0600

    Agregue validacion para administradores

---
+++ docker/www/public/controllers/AdminController.php
@@ -0,0 +1,24 @@
+﻿<?php
+require '../models/admin.php';
+
+// Comprueba si el método de la solicitud es POST por medio de fetch API
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nombre = trim($_POST['nombre']);
+    $pin = trim($_POST['pin']);
+
+    // Instancia de la clase Admin
+    $admin = new Admin();
+
+    // Verifica las credenciales del administrador
+    if ($admin->authenticate($nombre, $pin)) {
+        // Devuelve la respuesta según el rol
+        if ($admin->rol === 'administrador') {
+            echo 'admin_login';
+        } else {
+            echo 'user_login';
+        }
+    } else {
+        echo 'Nombre o PIN incorrecto.';
+    }
+}
+?>
